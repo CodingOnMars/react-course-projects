2775d32a3ef00bc097aaf4fc99b9c49b3306aa45
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-23 13:45:09 +0300

    Fetch data

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -3,8 +3,44 @@ import Loading from './Loading';
 import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    // NOTE: when fetch loading set setLoading to true. In case if useState(false).
+    // Technically, in our case, we don't need it here since we set up useState(true)
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      // console.log(tours); // returns an array with 5 tour objects, to see it we need to set up useEffect() first
+      setLoading(false); // hide title with 'Loading...'
+      setTours(tours); // show Tours component
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []); // [] - empty dependency array (list), run only once, on initial render
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
