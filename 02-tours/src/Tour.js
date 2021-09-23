d6e3f8cec2e909f1126a2765a5bb2d8c8452786c
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-23 14:00:33 +0300

    display Tours

--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4 className='tour-title'>{name}</h4>
+          <p className='tour-price'>${price}</p>
+        </div>
+        <p>{info}</p>
+        <button className='delete-btn'>not interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
