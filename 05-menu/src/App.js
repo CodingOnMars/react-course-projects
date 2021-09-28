6713e5a5aa11ca3d1fac054f57698c0bbf54a251
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-28 12:05:31 +0300

    Dynamic displaying of categories

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -3,10 +3,28 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// NOTES:
+// We want to get an array with unique categories
+// 1. First, we use map() method to get an array with categories
+// 2. Then we use new Set() to get unique values
+// 3. Since set returns an object, we wrap it to an array and since we wanted to have 'all' category, we add it in the beginning and then use spread operator to get remaining categories
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+// console.log(allCategories); // returns an array of 4 items with 'all' in the beginning
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
 
-  const [categroies, setCategroies] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -14,7 +32,7 @@ function App() {
         <div className='title'>
           <h2>Our menu</h2>
           <div className='underline'></div>
-          <Categories />
+          <Categories categories={categories} filterItems={filterItems} />
           <Menu items={menuItems} />
         </div>
       </section>
--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
-const Categories = () => {
-  console.log();
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className='btn-container'>
+      {/* Manual appraoch
+      
+      <button className='filter-btn' onClick={() => filterItems('breakfast')}>
+        Breakfast
+      </button>
+      <button className='filter-btn' onClick={() => filterItems('all')}>
+        All
+      </button> */}
+
+      {/* Dynamic approach */}
+      {categories.map((category, index) => {
+        return (
+          <button
+            className='filter-btn'
+            type='button'
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
