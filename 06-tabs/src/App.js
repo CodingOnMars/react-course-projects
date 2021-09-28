cc262a9d40564211db6d007c910b9035913734c6
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-28 14:38:25 +0300

    Display first job item

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -26,6 +26,34 @@ function App() {
     );
   }
 
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+        <div className='jobs-center'>
+          {/* btn-container */}
+          {/* job info */}
+          <article className='job-info'>
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className='job-dates'>{dates}</p>
+            {duties.map((duty, index) => {
+              return (
+                <div className='job-desc' key={index}>
+                  <FaAngleDoubleRight className='job-icon' />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+
   return <h2>jobs</h2>;
 }
 
