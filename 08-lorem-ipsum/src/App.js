afbdc16610247b4bd4beae4aa8b6e242fe727ead
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-30 15:26:12 +0300

    Added functionality to generate and display text

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -6,14 +6,26 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Submit form');
+    // NOTE: by default form returns a string
+    // console.log(typeof count);
+    let amount = parseInt(count);
+    // console.log(typeof amount); // now returns a number
+    if (count <= 0) {
+      amount = 0;
+    }
+
+    if (count > 8) {
+      amount = 8;
+    }
+    // NOTE: data returns an array with all items. To return amount equal to input value, we use slice() method
+    setText(data.slice(0, amount));
   };
 
   return (
     <section className='section-center'>
       <h3>Tired of boring lorem ipsum?</h3>
       <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>Paragraphs:</label>
+        <label htmlFor='amount'>Paragraphs (8 max):</label>
         <input
           type='number'
           name='amount'
@@ -26,14 +38,9 @@ function App() {
         </button>
       </form>
       <article className='lorem-text'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-          exercitationem.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-          exercitationem.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
