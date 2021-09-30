312b96bf8a18f2447b0e134150e779c134b3c9ca
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-30 14:31:24 +0300

    Added basic structure

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit form');
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+          exercitationem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+          exercitationem.
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
