1798c5056a500ee991961d3026c340791a741fec
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-07 11:37:56 +0300

    Set up context

--- 12-sidebar-modal/src/Home.js
+++ 12-sidebar-modal/src/Home.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { AppContext, useGlobalContext } from './context';
 
 const Home = () => {
+  const data = useGlobalContext();
+  // console.log(data); // returns AppContext.Provider value
+
   return (
     <main>
       <button className='sidebar-toggle'>
--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -1,1 +1,19 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+// NOTE: make sure to display children
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value='test value'>{children}</AppContext.Provider>
+  );
+};
+
+// Custom hook in case if we don't want to import useContext and AppContext too many times
+
+// NOTE: custom hooks as regular hooks must start with 'use' - useGlobalContext
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
