f1a03c7c1ad093b0a1c363a4d01ee7991777cf90
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-12 12:00:44 +0300

    Increase/decrease items amount functionality

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -10,6 +10,30 @@ const reducer = (state, action) => {
     };
   }
 
+  // If item id match, increase amount by 1
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  // If item id match, decrease amount by 1. If amount is 0, remove item
+  if (action.type === 'DECREASE') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+
   return state;
 };
 
