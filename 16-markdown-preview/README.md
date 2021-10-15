08dce618257a25f5651fb8ce0c7a0dc3d7dc884b
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-15 15:00:01 +0300

    Added setup files

---
+++ 16-markdown-preview/README.md
@@ -0,0 +1,5 @@
+## Markdown preview
+
+#### Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ 16-markdown-preview/src/App.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+function App() {
+  return <h2>Markdown preview project</h2>;
+}
+
+export default App;
