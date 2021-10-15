042baa7fdcfc6c5562b327acf2d2a4f6fd705a20
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-15 15:56:57 +0300

    Added setup files

---
+++ 17-random-person/src/App.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
+
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
+function App() {
+  return <h2>Random person</h2>;
+}
+
+export default App;
