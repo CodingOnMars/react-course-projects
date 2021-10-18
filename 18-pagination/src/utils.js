5815ae798da57d3b080eed9865c2dd714283df71
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-18 10:47:19 +0300

    Paginate setup

--- 18-pagination/src/utils.js
+++ 18-pagination/src/utils.js
@@ -1,3 +1,15 @@
-const paginate = () => {};
+const paginate = (followers) => {
+  const itemsPerPage = 9;
+  const pages = Math.ceil(followers.length / itemsPerPage); // returns 11,11 without Math.ceil, that's why we had to use it
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    // console.log(start);
+
+    // Start with 0, end with 9
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers; // returns an array of array
+};
 
 export default paginate;
