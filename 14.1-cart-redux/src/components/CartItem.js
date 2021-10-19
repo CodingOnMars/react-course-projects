4e4549e5a5afdfd5a13fb11f74d823a2d0c847a4
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-19 14:44:32 +0300

    Added Increase and decrease items functionality

--- 14.1-cart-redux/src/components/CartItem.js
+++ 14.1-cart-redux/src/components/CartItem.js
@@ -1,7 +1,15 @@
 import { connect } from 'react-redux';
 import { INCREASE, DECREASE, REMOVE } from '../actions';
 
-const CartItem = ({ img, title, price, amount, remove }) => {
+const CartItem = ({
+  img,
+  title,
+  price,
+  amount,
+  remove,
+  increase,
+  decrease,
+}) => {
   return (
     <div className='cart-item'>
       <img src={img} alt={title} />
@@ -13,13 +21,15 @@ const CartItem = ({ img, title, price, amount, remove }) => {
         </button>
       </div>
       <div>
-        <button className='amount-btn'>
+        {/* Increase amount */}
+        <button className='amount-btn' onClick={() => increase()}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
           </svg>
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        {/* Decrease amount */}
+        <button className='amount-btn' onClick={() => decrease()}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
           </svg>
@@ -32,10 +42,14 @@ const CartItem = ({ img, title, price, amount, remove }) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   // console.log(ownProps); // returns unique values for each item in the cart
 
-  const { id } = ownProps;
+  const { id, amount } = ownProps;
 
   // NOTE: we must use a fucntion to dispatch type, so the action will happen only if user click on Remove button. Otherwise action will fire on page load (in our case 3 times, because we have 3 items in the cart)
-  return { remove: () => dispatch({ type: REMOVE, payload: { id } }) };
+  return {
+    remove: () => dispatch({ type: REMOVE, payload: { id } }),
+    increase: () => dispatch({ type: INCREASE, payload: { id } }),
+    decrease: () => dispatch({ type: DECREASE, payload: { id, amount } }),
+  };
 };
 
 export default connect(null, mapDispatchToProps)(CartItem);
