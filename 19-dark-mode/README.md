24d09eb50962ea85c87d9ecc27b98ace39ec1470
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-23 14:15:42 +0300

    Basic setup for Article. Updated data.js and readme

--- 19-dark-mode/README.md
+++ 19-dark-mode/README.md
@@ -1,5 +1,7 @@
 ## Dark mode
 
+> Project inspired by personal blog of Dan Abramov.
+
 #### Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- 19-dark-mode/src/App.js
+++ 19-dark-mode/src/App.js
@@ -2,7 +2,21 @@ import data from './data';
 import Article from './Article';
 
 function App() {
-  return <h2>Dark mode</h2>;
+  return (
+    <main>
+      <nav>
+        <div className='nav-center'>
+          <h1>Overreacted</h1>
+          <div className='btn'>toggle</div>
+        </div>
+      </nav>
+      <section className='articles'>
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default App;
