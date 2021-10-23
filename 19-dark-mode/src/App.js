00e687c9d174f91967a42c1a366852f3b3023d8d
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-23 14:48:40 +0300

    Toggle between light and dark themes

--- 19-dark-mode/src/App.js
+++ 19-dark-mode/src/App.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react';
 import data from './data';
 import Article from './Article';
 
 function App() {
+  const [theme, setTheme] = useState('light-theme');
+
+  useEffect(() => {
+    // console.log(document.documentElement); // returns the whole html tag
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
   return (
     <main>
       <nav>
         <div className='nav-center'>
           <h1>Overreacted</h1>
-          <div className='btn'>toggle</div>
+          <div className='btn' onClick={toggleTheme}>
+            toggle
+          </div>
         </div>
       </nav>
       <section className='articles'>
