4ce6894f0be796ca88ae4357b80844abe8e5adf9
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-23 13:50:39 +0300

    Added setup files

---
+++ 19-dark-mode/src/App.js
@@ -0,0 +1,8 @@
+import data from './data';
+import Article from './Article';
+
+function App() {
+  return <h2>Dark mode</h2>;
+}
+
+export default App;
---
+++ 19-dark-mode/src/Article.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+const Article = () => {
+  return <h2>article component</h2>;
+};
